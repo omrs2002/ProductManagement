9a9f5595f128d01e8bc05d99d07ce56543a3055b
Omar Abuhadid <omrs2002@yahoo.com>
2022-06-16 17:47:36 +0300

    Add project files.

---
+++ src/ProductManagement.Domain.Shared/Products/ProductStockState.cs
@@ -0,0 +1,15 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProductManagement.Products
+{
+    public enum ProductStockState : byte
+    {
+        PreOrder,
+        InStock,
+        NotAvailable,
+        Stopped
+    }
+}
---
+++ src/ProductManagement.Domain/Products/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+using ProductManagement.Category;
+
+namespace ProductManagement.Products
+{
+    public class Product : FullAuditedAggregateRoot<Guid>
+    {
+        public Category.Category Category { get; set; }
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public bool IsFreeCargo { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public ProductStockState StockState { get; set; }
+    }
+
+}
